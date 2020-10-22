4cabb0906b0e4be60e739505f798a3bb66b7f82a
DJ Simmons <jiveeventsjax@gmail.com>
2020-10-21 20:10:34 -0400

    Fully Functional. Passing 15/16

---
+++ scripts.js
@@ -0,0 +1,90 @@
+let trailingResult = 0;
+let operationOptions = ['divide', 'multiply', 'subtract', 'add'];
+let workingOperation = "";
+function updateDisplay(input) {
+    let display = document.getElementById("display");
+    let secondaryDisplay = document.getElementById("secondaryDisplay");
+    if (display.innerHTML === "0" && operationOptions.indexOf(input) === -1) {
+        if (input === "decimal") {
+            display.innerHTML = "0.";
+        } else if (input === "negative-value") {
+            if (display.innerHTML.indexOf("-1") === -1) {
+                display.innerHTML = "-" + display.innerHTML
+            } else if (display.innerHTML.indexOf("-1" > -1)) {
+                display.innerHTML = display.innerHTML.slice(1, display.innerHTML.length);
+            }
+        } else {
+            display.innerHTML = input;
+        }
+    } else if (operationOptions.indexOf(input) >= 0) {
+        // console.log("Dealing with a operation");	
+        if (trailingResult === display.innerHTML) {
+            // Operand button pressed twice exeception	
+            workingOperation = input;
+        } else if (workingOperation === "") {
+            // Dealing without an operand	
+            workingOperation = input;
+            trailingResult = display.innerHTML;
+            secondaryDisplay.innerHTML = trailingResult;
+            display.innerHTML = 0;
+     } else {	
+      // Dealing with a set operand	
+      // console.log(display.innerHTML, " Dealing with set operand");	
+      trailingResult = calculate(trailingResult, display.innerHTML, workingOperation);	
+      secondaryDisplay.innerHTML = trailingResult;	
+      display.innerHTML = 0;	
+      workingOperation = input;	
+    }	
+  } else if (input === "equals") {	
+    display.innerHTML = calculate(trailingResult, display.innerHTML, workingOperation);	
+    trailingResult = 0;	
+    workingOperation = "";	
+    secondaryDisplay.innerHTML = trailingResult;	
+  } else if (input === "decimal") {	
+    // console.log('decimal clicked');	
+    if (display.innerHTML.indexOf(".") === -1) {	
+      display.innerHTML += ".";	
+    }
+        // console.log("decimal skipped because one is already in there");
+    } else if (input === "negative-value") {
+        // console.log("negative-value selected");
+        if (display.innerHTML.indexOf("-1") === -1) {
+            display.innerHTML = "-" + display.innerHTML
+        } else if (display.innerHTML.indexOf("-1" > -1)) {
+            display.innerHTML = display.innerHTML.slice(1, display.innerHTML.length);
+        }
+    } else {
+        display.innerHTML += input;
+    }
+    // console.log(trailingResult, "<= trailingResult", display.innerHTML, " <= display.innerHTML", workingOperation, "<= workingOperation")
+}
+
+function clearDisplay() {
+    let display = document.getElementById("display");
+    display.innerHTML = 0;
+    trailingResult = 0;
+}
+
+function calculate(firstNumber, secondNumber, operation) {
+    let result;
+    firstNumber = parseFloat(firstNumber);
+    secondNumber = parseFloat(secondNumber);
+    switch (operation) {
+        case "add":
+            result = firstNumber + secondNumber; 
+            break;
+        case "subtract":
+            result = firstNumber - secondNumber; 
+            console.log("subtract switch!")
+            break;
+        case "multiply":
+            result = firstNumber * secondNumber; 
+            break;
+        case "divide":
+            result = firstNumber / secondNumber; 
+            break;
+        default:
+        console.log("switch statement failed!")
+    }
+    return result.toString();
+}
